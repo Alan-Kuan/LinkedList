f39424944ec26df7c77bc02caceab3930dfe706f
Alan-Kuan <kuanhaoan@gmail.com>
2020-02-06 14:59:23 +0800

    Added demo2 and modified Makefile to also clean run_demo*.

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 CC = g++
 
-.PHONY: demo, clean
+.PHONY: demo, demo2, clean
 
 demo: examples/linked_list_demo.cpp include/linked_list.hpp src/linked_list.tpp
 	$(CC) -o run_demo $<
 
+demo2: examples/linked_list_demo2.cpp include/linked_list.hpp src/linked_list.tpp
+	$(CC) -o run_demo2 $<
+
 clean:
 	-rm *.o
+	-rm run_demo*
---
+++ examples/linked_list_demo2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../include/linked_list.hpp"
+
+using namespace std;
+
+using namespace ll;
+
+int main(void){
+
+	LinkedList<int> list = {1, 2, 3, 4, 5};
+
+	list[3] = 3;
+
+	cout << list[3] << endl;
+
+	const LinkedList<double> list2 = {0.1, 0.2, 0.3};
+
+	cout << list2[0] << endl;
+
+	return 0;
+
+}
